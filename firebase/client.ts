98abec7e41e6ebff856e301b84dd17a1750ffdb0
jeevanshib <jeevanshi.jb@gmail.com>
2025-03-31 01:31:53 +0530

    Interview backend

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,11 +3,6 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAuOHj0DtnEn89HL0gE31cW21n6ekx8UqI",
     authDomain: "convoquest-191ba.firebaseapp.com",
